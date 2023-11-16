68be37a0260fb2092480a3003cfac265dd90a72b
Flowmikro <cloplop091@gmail.com>
2023-11-16 04:38:19 +0300

    Файл заполняет БД данными

---
+++ app/db_entries.py
@@ -0,0 +1,27 @@
+# Заполняет БД записями
+from .database import collection
+
+db_templates = [
+    {
+        "name": "User registration by phone number and email",
+        "user_phone": "phone",
+        "user_email": "email",
+    },
+    {
+        "name": "Employee's date of birth",
+        "employee_name": "text",
+        "date_of_birth": "date",
+    },
+    {
+        "name": "Employee_phone_number",
+        "employee_name": "text",
+        "employee_phone_number": "phone",
+    },
+    {
+        "name": "User_blog",
+        "first_name": "text",
+        "user_post": "text",
+    },
+]
+
+collection.insert_many(db_templates)
