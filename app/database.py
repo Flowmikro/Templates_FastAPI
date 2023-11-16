dfd431f238c1d50fc9ed947143f90d84a1a5faee
Flowmikro <cloplop091@gmail.com>
2023-11-16 04:40:19 +0300

    Настойка бд

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGODB_URL = "mongodb://localhost:27017/app/mongodb"
+MONGODB_URL = "mongodb://localhost:27017/mongodb"
 client = AsyncIOMotorClient(MONGODB_URL)
 db = client['mongodb']
 collection = db['templates']
